c1ace9b82c565ad0ed36959896a4af8ba0bb56f6
Pavel Kalugin <kalugin@lps.u-psud.fr>
2026-01-26 16:15:24 +0100

    Making SpaceGroupQuotient an independent type, delegating calls to the internal FiniteGroup field.

--- src/macros.jl
+++ src/macros.jl
@@ -7,19 +7,21 @@ Generate thin forwarding methods for each function name in `targets`, delegating
 calls on `TypeName` instances to the specified field. 
 """
 macro delegate(source, targets)
-    # source is an expression like SpaceGroupQuotient.group
-    typename = source.args[1]
-    # Extract the field name from the QuoteNode or Symbol
-    fieldname = source.args[2] isa QuoteNode ? source.args[2].value : source.args[2]
+    # source is like SpaceGroupQuotient{N, T}.group
+    # or just SpaceGroupQuotient.group
     
-    # Handle both single functions and tuples of functions
+    type_expr = source.args[1] 
+    field = source.args[2] isa QuoteNode ? source.args[2].value : source.args[2]
+    
+    # Extract functions to delegate
     funcs = targets isa Expr && targets.head == :tuple ? targets.args : [targets]
     
     defs = Any[]
     for f in funcs
         push!(defs, quote
-            @inline $(esc(f))(obj::$(esc(typename)), args...; kwargs...) = 
-                $(esc(f))(getfield(obj, $(QuoteNode(fieldname))), args...; kwargs...)
+            @inline function $(esc(f))(obj::$(esc(type_expr)), args...; kwargs...)
+                return $(esc(f))(getfield(obj, $(QuoteNode(field))), args...; kwargs...)
+            end
         end)
     end
     return Expr(:block, defs...)
--- src/spacegroupquotient.jl
+++ src/spacegroupquotient.jl
@@ -12,8 +12,8 @@ particular, the representation of symmorphic and non-symmorphic space groups is 
 
 # Constructors
 - SpaceGroupQuotient{N,T}(): Construct the trivial space group (P1) of dimension N 
-- SpaceGroupQuotient{N,T}(gen): Constructs the space group using the generating set `gen` 
-  (which should be an iterable of `SpaceGroupElement{N, T}`)
+- SpaceGroupQuotient{N,T}(gen::AbstractVector{SpaceGroupElement{N,T}}): Constructs the space 
+  group using the generating set `gen` 
 
 # Examples
 ```julia-repl
@@ -36,7 +36,19 @@ julia> p2mg=SpaceGroupQuotient([g1, g2])
 SpaceGroupQuotient (dimension 2, order 4)
 ```
 """
-const SpaceGroupQuotient{N,T} = FiniteGroup{SpaceGroupElement{N,T}}
+struct SpaceGroupQuotient{N,T<:Integer} 
+    group::FiniteGroup{SpaceGroupElement{N,T}}
+end
+
+SpaceGroupQuotient{N, T}() where {N,T<:Integer} = 
+    SpaceGroupQuotient{N, T}(FiniteGroup{SpaceGroupElement{N,T}}())
+
+function SpaceGroupQuotient(gen::AbstractVector{SpaceGroupElement{N,T}}) where {N,T<:Integer}
+    group=FiniteGroup{SpaceGroupElement{N,T}}(gen)
+    SpaceGroupQuotient{N, T}(group)
+end
+
+@delegate SpaceGroupQuotient.group (Base.length, Base.iterate, Base.eltype)
 
 function Base.show(io::IO, ::MIME"text/plain", G::SpaceGroupQuotient{N,T}) where {N,T}
     print(io, "SpaceGroupQuotient (dimension $N, order $(length(G)))")
